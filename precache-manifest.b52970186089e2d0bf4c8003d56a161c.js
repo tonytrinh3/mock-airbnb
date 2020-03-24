f8a001697b90562b3a297289c85fa02e11b0d837
tony trinh <tony.w.trinh3@gmail.com>
2020-03-23 23:50:07 -0700

    Updates

---
+++ precache-manifest.b52970186089e2d0bf4c8003d56a161c.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc40edd246a41c35d53cbf9a5b7a6c5",
+    "url": "/mock-airbnb/index.html"
+  },
+  {
+    "revision": "6ee661c1ca9563b46ca5",
+    "url": "/mock-airbnb/static/css/main.b967cedb.chunk.css"
+  },
+  {
+    "revision": "f9258a380c13fb2104ee",
+    "url": "/mock-airbnb/static/js/2.1f62f2e2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mock-airbnb/static/js/2.1f62f2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee661c1ca9563b46ca5",
+    "url": "/mock-airbnb/static/js/main.23a602cf.chunk.js"
+  },
+  {
+    "revision": "9ca7ee566d6cc3406b19",
+    "url": "/mock-airbnb/static/js/runtime-main.f0956324.js"
+  },
+  {
+    "revision": "e9797f0c33f22dd6115a5d5736d316fd",
+    "url": "/mock-airbnb/static/media/Belarus.e9797f0c.jpg"
+  },
+  {
+    "revision": "bb579a8c85a7fac1764784684fa714fa",
+    "url": "/mock-airbnb/static/media/Cuba.bb579a8c.jpg"
+  },
+  {
+    "revision": "d3dc28c09a37e1ad946aa6c038f48b52",
+    "url": "/mock-airbnb/static/media/Greece.d3dc28c0.jpg"
+  },
+  {
+    "revision": "99cda844a15d2553dfe3acf48475986e",
+    "url": "/mock-airbnb/static/media/Montenegro.99cda844.jpg"
+  },
+  {
+    "revision": "4befde1564593f6734706d7ac25dd868",
+    "url": "/mock-airbnb/static/media/Portugal.4befde15.jpg"
+  },
+  {
+    "revision": "4485fec57d45b91f91e49d41b5defd63",
+    "url": "/mock-airbnb/static/media/South_Africa.4485fec5.jpg"
+  },
+  {
+    "revision": "8dea1fb5b6197dfb2a07c939f11d1823",
+    "url": "/mock-airbnb/static/media/Spain.8dea1fb5.jpg"
+  },
+  {
+    "revision": "013920202755cf7a0b22f75723efa17b",
+    "url": "/mock-airbnb/static/media/United_States.01392020.jpg"
+  },
+  {
+    "revision": "c77712c7a1b5dd42979a58e307757525",
+    "url": "/mock-airbnb/static/media/adventures-image.c77712c7.jpg"
+  },
+  {
+    "revision": "cc789d7362513e46fa07752244f5d037",
+    "url": "/mock-airbnb/static/media/airbnb-logo.cc789d73.png"
+  },
+  {
+    "revision": "6b5a49b7578c7efb4055c56a0e940190",
+    "url": "/mock-airbnb/static/media/airbnb-plus2.6b5a49b7.jpg"
+  },
+  {
+    "revision": "7f55b72b7baa292e5d81b92a5f33ffb7",
+    "url": "/mock-airbnb/static/media/canada-adventure.7f55b72b.jpg"
+  },
+  {
+    "revision": "a3e48f33ec84ad267412a0c0d95cf50f",
+    "url": "/mock-airbnb/static/media/cape_town.a3e48f33.PNG"
+  },
+  {
+    "revision": "92fa074d1a89daea05aa8bbd64c34d92",
+    "url": "/mock-airbnb/static/media/chicago.92fa074d.PNG"
+  },
+  {
+    "revision": "31101f712a6b402a03ab19aab7bf8d56",
+    "url": "/mock-airbnb/static/media/colombia.31101f71.PNG"
+  },
+  {
+    "revision": "82187c106114ba97a336e5021305c89c",
+    "url": "/mock-airbnb/static/media/experiences-image.82187c10.jpg"
+  },
+  {
+    "revision": "00d083a12214da9bba3c846adcb39608",
+    "url": "/mock-airbnb/static/media/hong-kong.00d083a1.jpg"
+  },
+  {
+    "revision": "003a04b9eb5336d2a4bce7a49e426bc7",
+    "url": "/mock-airbnb/static/media/iceland.003a04b9.PNG"
+  },
+  {
+    "revision": "3ef33f4bc2961c02bbb8425b994117c0",
+    "url": "/mock-airbnb/static/media/india-adventure.3ef33f4b.jpeg"
+  },
+  {
+    "revision": "2d1890c6d01da16e2b91d3a2f9cc79ee",
+    "url": "/mock-airbnb/static/media/indonesia.2d1890c6.PNG"
+  },
+  {
+    "revision": "98d4a9ea3fc392b88a7cb8fed6b73ced",
+    "url": "/mock-airbnb/static/media/london.98d4a9ea.PNG"
+  },
+  {
+    "revision": "541657075227491dfed72bc9916f2713",
+    "url": "/mock-airbnb/static/media/madagascar-adventure.54165707.jpeg"
+  },
+  {
+    "revision": "bc703e06a0c193c5074363a5a5478b39",
+    "url": "/mock-airbnb/static/media/mexico.bc703e06.PNG"
+  },
+  {
+    "revision": "9e9294ab2f48c06d7ae4dfd349f5c7ed",
+    "url": "/mock-airbnb/static/media/russia-adventure.9e9294ab.jpeg"
+  },
+  {
+    "revision": "cb0ff3dbeb0875b8b9aa9ffec896749c",
+    "url": "/mock-airbnb/static/media/stays-image.cb0ff3db.jpg"
+  },
+  {
+    "revision": "4630536a85742fb6225bce5014623dc4",
+    "url": "/mock-airbnb/static/media/united_kingdom.4630536a.PNG"
+  },
+  {
+    "revision": "ff168bd4b92fd133688640f3c217ecc3",
+    "url": "/mock-airbnb/static/media/united_states.ff168bd4.PNG"
+  },
+  {
+    "revision": "77f0801f9353357a3568505213cf0ab9",
+    "url": "/mock-airbnb/static/media/united_states2.77f0801f.PNG"
+  },
+  {
+    "revision": "e733299e652c0a333a5c2e41e5a2a08b",
+    "url": "/mock-airbnb/static/media/united_states3.e733299e.PNG"
+  },
+  {
+    "revision": "3791f43e6ecc730ede87d29902c626de",
+    "url": "/mock-airbnb/static/media/us-adventure.3791f43e.jpg"
+  }
+]);
