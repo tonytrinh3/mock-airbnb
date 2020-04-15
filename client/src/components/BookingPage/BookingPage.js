81e1bc7d0dc5d240146718e6cdfd829cd2c7a335
tony trinh <tony.w.trinh3@gmail.com>
2020-04-14 22:37:57 -0700

    booking page routed up to reducer

--- client/src/components/BookingPage/BookingPage.js
+++ client/src/components/BookingPage/BookingPage.js
@@ -13,17 +13,35 @@ import ExploreOptions from './ExploreOptions';
 import Footer from '../Footer';
 
 import {connect} from 'react-redux';
-
+import {fetchBooking} from '../../actions/index';
 
 
 
 
 class BookingPage extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchBooking(this.props.match.params.id);
+    }
+
+
+    renderStuff(){
+        return(
+            <div className="aewf" style={{color: "red"}}>{this.props.booking.title}</div>
+        )
+
+    }
+
     render(){
+        if (!this.props.booking){
+            return (<div className="awef">Loading!</div>);
+        }
+        console.log(this.props.booking);
         return(
             <div className="booking-page">
                 <Navigation/>
                 <PictureHeader/>
+                {this.renderStuff()}
                 <BookingCard/>
                 <Amenities/>
                 <SleepingArrangement/>
@@ -40,11 +58,15 @@ class BookingPage extends React.Component{
 
 };
 
+
+//so you can pull it one time and just pass it down as props...
+
 const mapStateToProps = (state,ownProps) =>{
     console.log(ownProps);
+    console.log(state);
     return{
-        state
+        booking: state.bookings[ownProps.match.params.id]
     }
 }
 
-export default connect(mapStateToProps)(BookingPage);
+export default connect(mapStateToProps,{fetchBooking})(BookingPage);
