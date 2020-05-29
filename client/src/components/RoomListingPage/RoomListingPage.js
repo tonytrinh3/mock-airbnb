75f81621380374c846ce32e116591b677b790ab2
tony trinh <tony.w.trinh3@gmail.com>
2020-05-29 14:22:39 -0700

    working on listing page

--- client/src/components/RoomListingPage/RoomListingPage.js
+++ client/src/components/RoomListingPage/RoomListingPage.js
@@ -9,6 +9,8 @@ import avgReview from '../avgReview';
 
 class RoomListingPage extends React.Component{
 
+
+    
     //then you call the action to use in here
     componentDidMount(){
         this.props.fetchBookings();
@@ -16,42 +18,44 @@ class RoomListingPage extends React.Component{
   
     renderSpaceType(space_type, home_type){
         console.log(space_type);
-        return <p className = "listings__listing__space-type">{(space_type = "entire_space") ? `Entire ${home_type}` : `Private Room` }</p>
+        return <p className = "listings__listing__description__space-type">{(space_type = "entire_space") ? `Entire ${home_type}` : `Private Room` }</p>
     }
 
     renderAmenities(amenities){
         console.log(amenities);
         return amenities.map(amenity=>{
-            return <li className="listings__listing__amenities__amenity">{amenity}</li>
+            return <li className="listings__listing__description__amenities__amenity">{amenity}</li>
         })
     }
 
     renderHomeSpecs(home_specs){
         return(
-            <ul className="listings__listing__home-specs">
-                <li className="listings__listing__home-specs__spec">{home_specs.numGuests} guests</li>  
-                <li className="listings__listing__home-specs__spec">{home_specs.numBedrooms} bedrooms</li> 
-                <li className="listings__listing__home-specs__spec">{home_specs.numBeds} beds</li> 
-                <li className="listings__listing__home-specs__spec">{home_specs.numBaths.private ? `${home_specs.numBaths.private} baths`:`${home_specs.numBaths.shared} shared baths`}</li> 
+            <ul className="listings__listing__description__home-specs">
+                <li className="listings__listing__description__home-specs__spec">{home_specs.numGuests} guests</li>  
+                <li className="listings__listing__description__home-specs__spec">{home_specs.numBedrooms} bedrooms</li> 
+                <li className="listings__listing__description__home-specs__spec">{home_specs.numBeds} beds</li> 
+                <li className="listings__listing__description__home-specs__spec">{home_specs.numBaths.private ? `${home_specs.numBaths.private} baths`:`${home_specs.numBaths.shared} shared baths`}</li> 
             </ul>
         )
     }
-
+    //· 14 days
     renderList(){
         return this.props.bookings.map(booking=>{
             return (
                 <div className="listings__listing" key = {booking.id}>
                     <div className="listings__listing__description">
-                        <Link to={`/booking/${booking.id}`} className="listings__listing__title"> {booking.title}</Link>
-                        <div className="superhost-tag">superhost</div>
+                        
+                        <div className="listings__listing__description__superhost-tag superhost-tag">superhost</div>
                         {this.renderSpaceType(booking.space_type, booking.home_type)}
-                        <p className="listings__listing__home-type">{booking.home_type}</p>
-                        <p className="listings__listing__price">${booking.price} / night</p>
-                        <p className="wef">{avgReview(booking.reviews,"overall")}</p>
+                        <p className="listings__listing__description__reviews">{avgReview(booking.reviews,"overall")}</p>
+                        <Link className="listings__listing__description__title header-medium" to={`/booking/${booking.id}`} > {booking.title}</Link>
+                        
+                        
                         {this.renderHomeSpecs(booking.home_specs)}
-                        <ul className="listings__listing__amenities">{this.renderAmenities(booking.amenities)}</ul>  
+                        <ul className="listings__listing__description__amenities">{this.renderAmenities(booking.amenities)}</ul>  
+                        <p className="listings__listing__description__price"><span >${booking.price}</span> / night</p>
                     </div>
-                    <img src={require(`../../img/booking-page/listing-${booking.id}/img-1.jpg`)} alt="" className="listings__listing__img"/>
+                    <img className="listings__listing__img" src={require(`../../img/booking-page/listing-${booking.id}/img-1.jpg`)} alt="" />
                 </div>
             )
         })
